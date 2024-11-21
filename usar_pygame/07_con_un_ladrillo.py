e7d7b2bb15d76d1ddcf19151fbb9bb76d7912489
Emilio <elprofesoremilio@gmail.com>
2024-11-21 10:46:04 +0100

    ies

--- usar_pygame/07_con_un_ladrillo.py
+++ usar_pygame/07_con_un_ladrillo.py
@@ -13,7 +13,7 @@ pygame.display.set_caption("Dibujando un círculo con Pygame")
 # Colores (RGB)
 blanco = (255, 255, 255)
 naranja = (255, 150, 0)
-turquesa = (0,255,255)
+azul = (0,0,255)
 negro = (0, 0, 0)
 rojo = (255,0,0)
 
@@ -37,6 +37,20 @@ barra_x = ancho//2 - barra_ancho//2
 barra_y = 550
 barra_velocidad = 10
 
+# Configuración del ladrillo
+
+ladrillos = []
+
+ladrillos.append([300,200,50,10,True])
+ladrillos.append([355,200,50,10,True])
+ladrillos.append([410,200,50,10,True])
+ladrillos.append([465,200,50,10,True])
+ladrillos.append([245,200,50,10,True])
+ladrillos.append([190,200,50,10,True])
+ladrillos.append([135,200,50,10,True])
+ladrillos.append([80,200,50,10,True])
+ladrillos.append([25,200,50,10,True])
+
 # Bucle principal
 corriendo = True
 while corriendo:
@@ -52,8 +66,14 @@ while corriendo:
     pygame.draw.circle(pantalla, naranja, centro, pelota_radio)
 
     # Dibujar barra
-    posicion_barra = (barra_x, barra_y, barra_ancho, barra_alto)
-    pygame.draw.rect(pantalla,negro,posicion_barra)
+    barra_posicion = (barra_x, barra_y, barra_ancho, barra_alto)
+    pygame.draw.rect(pantalla, negro, barra_posicion)
+
+    for ladrillo in ladrillos:
+        # Dibujar ladrillo
+        if ladrillo[4]:
+            ladrillo_posicion = (ladrillo[0],ladrillo[1],ladrillo[2],ladrillo[3])
+            pygame.draw.rect(pantalla, azul, ladrillo_posicion)
 
     # Actualizar barra
     keys = pygame.key.get_pressed()
@@ -62,15 +82,22 @@ while corriendo:
     if keys[pygame.K_LEFT]:
         barra_x -= barra_velocidad
 
+    # Compruebo ladrillo
+    for ladrillo in ladrillos:
+        # Dibujar ladrillo
+        if ladrillo[4]:
+            if (pelota_x+pelota_radio >= ladrillo[0]) and (pelota_x-pelota_radio <= ladrillo[0] + ladrillo[2]):
+                if ladrillo[1] <= pelota_y + pelota_radio <= ladrillo[1] + ladrillo[3]:
+                    ladrillo[4] = False
+                    pelota_velocidad_y = -pelota_velocidad_y
+
     # Actualizar pelota
     pelota_x += pelota_velocidad_x
     pelota_y += pelota_velocidad_y
 
-    if (pelota_x-pelota_radio >= barra_x) and (pelota_x+pelota_radio <= barra_x + barra_ancho):
-        if pelota_y + pelota_radio >= barra_y:
+    if (pelota_x+pelota_radio >= barra_x) and (pelota_x-pelota_radio <= barra_x + barra_ancho):
+        if barra_y <= pelota_y + pelota_radio <= barra_y + barra_alto :
             pelota_velocidad_y = -(abs(pelota_velocidad_y))
-
-
 
     if (pelota_x > ancho - pelota_radio) or (pelota_x - pelota_radio < 1):
         pelota_velocidad_x = -pelota_velocidad_x
